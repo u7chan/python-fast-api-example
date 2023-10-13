6f4a4042f849a701caa09b5cc31f012038be2c83
una <unagamix@gmail.com>
2023-10-14 00:38:42 +0900

    fix: health-check

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/health-check")
+async def health_check():
+    return {"message": "OK"}
