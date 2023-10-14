5871d82e44a3630645968ae2eb696145273c9fb5
una <unagamix@gmail.com>
2023-10-15 00:15:25 +0900

    feat: database migration by alembic

---
+++ src/infrastructure/database/models.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, String
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+
+
+@as_declarative()
+class Base:
+    @declared_attr
+    def created_at(cls):
+        return Column(DateTime, default=datetime.now(), nullable=False)
+
+    @declared_attr
+    def updated_at(cls):
+        return Column(
+            DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=False
+        )
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(String, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    nickname = Column(String)
