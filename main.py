b4fdf0f653fe60c224696e3750b2aff17d0ee9d1
una <unagamix@gmail.com>
2023-10-17 01:03:02 +0900

    feat: fast api router

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
 from fastapi import FastAPI, Depends
 
-from domain.usecase import CreateUserUseCase, FetchUsersUseCase
-from di import inject_create_user_usecase, inject_fetch_users_usecase
+from presentation import user_endpoints
 
 app = FastAPI()
+app.include_router(user_endpoints.router)
 
 
 @app.get("/health-check")
 async def health_check():
     return {"message": "OK"}
-
-
-@app.get("/users")
-async def get_users(
-    fetch_users_usecase: FetchUsersUseCase = Depends(inject_fetch_users_usecase),
-):
-    return fetch_users_usecase.execute()
-
-
-@app.post("/users")
-async def create_user(
-    create_user_usecase: CreateUserUseCase = Depends(inject_create_user_usecase),
-):
-    return create_user_usecase.execute()
---
+++ presentation/user_endpoints.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends, status
+
+from domain.usecase import CreateUserUseCase, FetchUsersUseCase
+from di import inject_create_user_usecase, inject_fetch_users_usecase
+
+
+router = APIRouter()
+
+
+@router.post(
+    "/user",
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_user(
+    create_user_usecase: CreateUserUseCase = Depends(inject_create_user_usecase),
+):
+    return create_user_usecase.execute()
+
+
+@router.get("/users", status_code=status.HTTP_200_OK)
+async def fetch_users(
+    fetch_users_usecase: FetchUsersUseCase = Depends(inject_fetch_users_usecase),
+):
+    return fetch_users_usecase.execute()
