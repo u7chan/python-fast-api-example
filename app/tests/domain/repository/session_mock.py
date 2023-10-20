549e4df356fa83658dc5ce5fe527450fc55d1cd4
una <unagamix@gmail.com>
2023-10-20 23:40:16 +0900

    feat: user repository save tests

--- app/tests/domain/repository/session_mock.py
+++ app/tests/domain/repository/session_mock.py
@@ -1,17 +1,51 @@
-class SessionMock:
-    all_value: list[any] = []
-    raise_query: any = None
+from typing import Any, Self
 
-    def query(self, _class) -> "SessionMock":
+
+class SessionMock:
+    raise_query: Any = None
+
+    one_return_value: Any = None
+    all_return_value: list[Any] = []
+    add_value: Any = None
+
+    query_call_count = 0
+    filter_by_count = 0
+    order_by_call_count = 0
+    limit_call_count = 0
+    one_call_count = 0
+    all_call_count = 0
+    add_call_count = 0
+    commit_call_count = 0
+
+    def query(self, class_type: Any) -> Self:
+        self.query_call_count += 1
         if not self.raise_query is None:
             raise self.raise_query
         return self
 
-    def order_by(self, _sort) -> "SessionMock":
+    def filter_by(self, **args: Any) -> Self:
+        self.filter_by_count += 1
         return self
 
-    def limit(self, _limit) -> "SessionMock":
+    def order_by(self, sort: Any) -> Self:
+        self.order_by_call_count += 1
         return self
 
-    def all(self) -> any:
-        return self.all_value
+    def limit(self, limit: Any) -> Self:
+        self.limit_call_count += 1
+        return self
+
+    def one(self) -> Any:
+        self.one_call_count += 1
+        return self.one_return_value
+
+    def all(self) -> list[Any]:
+        self.all_call_count += 1
+        return self.all_return_value
+
+    def add(self, data: Any):
+        self.add_call_count += 1
+        self.add_value = data
+
+    def commit(self):
+        self.commit_call_count += 1
--- app/tests/domain/repository/test_user_repository.py
+++ app/tests/domain/repository/test_user_repository.py
@@ -18,12 +18,13 @@ class TestUserRepository:
     def test_should_be_fetch_empty(self):
         # Given
         excepted = []
-        self.session_mock.all_value = excepted
+        self.session_mock.all_return_value = excepted
 
         # When
         actual = self.repository.fetch_all()
 
         # Then
+        assert self.session_mock.all_call_count == 1
         assert actual == excepted
 
     def test_should_be_fetch_multiple_item(self):
@@ -32,7 +33,7 @@ class TestUserRepository:
             User(name="#name1", email="#email1"),
             User(name="#name2", email="#email2"),
         ]
-        self.session_mock.all_value = list(
+        self.session_mock.all_return_value = list(
             map(lambda entity: UserDto.from_entity(entity), users)
         )
 
@@ -40,6 +41,7 @@ class TestUserRepository:
         actual = self.repository.fetch_all()
 
         # Then
+        assert self.session_mock.all_call_count == 1
         assert len(actual) == len(users)
         for i, expected in enumerate(users):
             assert actual[i] == expected
@@ -55,3 +57,36 @@ class TestUserRepository:
 
         # Then
         assert str(actual.value) == expected
+
+    def test_should_be_append_data(self):
+        # Given
+        user = User(name="#name", email="#email")
+        excepted = UserDto.from_entity(user)
+
+        # When
+        actual = self.repository.save(user=user)
+        add_args = self.session_mock.add_value
+
+        # Then
+        assert self.session_mock.add_call_count == 1
+        assert self.session_mock.commit_call_count == 1
+        assert actual.id == add_args.id
+        assert actual.name == excepted.name
+        assert actual.email == excepted.email
+        assert add_args.name == excepted.name
+        assert add_args.email == excepted.email
+
+    def test_should_be_update_data(self):
+        # Given
+        user = User(id="#id", name="#name", email="#email")
+        excepted = UserDto.from_entity(user)
+        self.session_mock.one_return_value = excepted
+
+        # When
+        actual = self.repository.save(user=user)
+
+        # Then
+        assert self.session_mock.one_call_count == 1
+        assert self.session_mock.commit_call_count == 1
+        assert actual.name == excepted.name
+        assert actual.email == excepted.email
