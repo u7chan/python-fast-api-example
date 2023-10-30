1b031519aef3beea3e69e5e66bb37c4806136d33
una <unagamix@gmail.com>
2023-10-31 00:54:41 +0900

    fix: add tests

--- app/tests/domain/repository/test_account_repository.py
+++ app/tests/domain/repository/test_account_repository.py
@@ -1,5 +1,7 @@
 import pytest
 
+from sqlalchemy.exc import NoResultFound
+
 from app.domain.entity.account import Account
 from app.domain.repository.account_repository import AccountRepository
 from app.infrastructure.database.models import AccountDao
@@ -35,3 +37,39 @@ class TestAccountRepository:
         assert actual.user_id == excepted.user_id
         assert actual.login_id == excepted.login_id
         assert actual.password_hash == excepted.password_hash
+
+    def test_should_be_find_by_login_id(self):
+        # Given
+        excepted = Account(
+            user_id="#user_id", login_id="#login_id", password="#password"
+        )
+        self.session.one__mock.return_value(AccountDao.from_entity(excepted))
+
+        # When
+        actual = self.repository.find_by_login_id(login_id="#login_id")
+
+        # Then
+        assert actual == excepted
+
+    def test_should_be_find_by_login_id_not_found(self):
+        # Given
+        excepted = None
+        self.session.one__mock.raise_error(NoResultFound)
+
+        # When
+        actual = self.repository.find_by_login_id(login_id="#login_id")
+
+        # Then
+        assert actual == excepted
+
+    def test_should_be_find_by_login_id_exception(self):
+        # Given
+        excepted = "test"
+        self.session.one__mock.raise_error(Exception(excepted))
+
+        # When
+        with pytest.raises(Exception) as actual:
+            self.repository.find_by_login_id(login_id="#login_id")
+
+        # Then
+        assert str(actual.value) == excepted
